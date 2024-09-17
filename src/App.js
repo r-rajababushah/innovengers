6cf07ab38de60631ed22a7bf2e7d73c14b24de54
Nikhil Patel <nikhilp9956@gmail.com>
2024-09-17 17:44:58 +0530

    HELLO NEW WEBSITE

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./Home/Home";
-import Hotels from "./Hotels/Hotels";
-import Layout from "./Layout/Layout";
-import Resturants from "./Resturants/Resturants";
+import Innovengers from "./Innovengers/Innovengers";
+import Navigation from "./Innovengers/Navigation";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path='/hotels' element={<Hotels />} />
-                    <Route path='/resturants' element={<Resturants />} />
+                <Route path="/" element={<Navigation />}>
+                    <Route index element={<Innovengers />} />
                 </Route>
             </Routes>
         </BrowserRouter>
