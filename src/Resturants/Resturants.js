fdf69ac0a6bd21d6c095f1d64cc7de16f1ace897
Rajababu Shah <rajababushah.in@gmail.com>
2024-09-18 16:11:46 +0530

    Resturants added

--- src/Resturants/Resturants.js
+++ src/Resturants/Resturants.js
@@ -1,7 +1,56 @@
+import "./Resturants.css";
+
 export default function Resturants(){
     return (
-        <>
-            This is in Resturants ;
-        </>
+        <div className="resturants">
+            <div className="cardP">
+                    <img src="food1.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>Fiona, Mumbai</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food2.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>Namak, Mumbai</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food3.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>Tamra, New Delhi</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food4.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>Spice Art, New Delhi</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food5.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>Karavalli, Bengaluru</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food6.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>loya-at-taj-west-end, Bengaluru</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food7.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>diya-dinning, Gurugram</b></h4> 
+                    </div>
+            </div> 
+            <div className="cardP">
+                    <img src="food8.jpg" id="place1" className="placeImg" alt="#" />
+                    <div className="container">
+                        <h4><b>Panchgaon, Gurugram</b></h4> 
+                    </div>
+            </div> 
+        </div>
     )
 }
