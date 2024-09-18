866a3364d82c160c4bf4c843d80cbdada23ac7af
Rajababu Shah <rajababushah.in@gmail.com>
2024-09-18 17:49:28 +0530

    U

--- src/Bookings/Bookings.js
+++ src/Bookings/Bookings.js
@@ -3,86 +3,27 @@ import './Bookings.css';
 export default function Bookings(){
     return (
         <div className="bookings">
-            <table id="contact" border="0" width="100%" cellpadding="0" cellspacing="0" bgcolor="#292929">
-        <tr>
-            <td>
-                <table border="0" width="85%" cellpadding="15" cellspacing="0" align="center">
+            <div className="bookinghead">
+                <h3>BOOKING</h3>
+                <hr style={{width: '400px'}} />
+            </div>
+            <div className="bookingForm">
+                <table>
                     <tr>
-                        <td height="180" align="center" valign="middle" colspan="1">
-                                <font face="arial" color="#f3971b" size="6">
-                                     BOOKING
-                                </font>
-                                <hr width="70" color="#f3971b" />
-                        
-                        </td>
+                        <td style={{color: 'white'}}>Name</td>
+                        <td><input type="text" name="name" id="name" /></td>
                     </tr>
                     <tr>
-                        <td align="center" valign="top">
-                           <table border="0" width="50%" cellpadding="15" cellspacing="0" align="center" bgcolor="#353535">
-                               <tr>
-                                   <td width="30%">
-                                       <font face="arial" size="4" color="#ffffff">
-                                           Name
-                                       </font>
-                                   </td>
-                                   <td width="70%">
-                                    <font face="arial" size="4" color="#ffffff">
-                                        <input type="text" size="40" />
-                                    </font>
-                                    </td>
-                               </tr>
-                               <tr>
-                                <td width="30%">
-                                    <font face="arial" size="4" color="#ffffff">
-                                        Email
-                                    </font>
-                                </td>
-                                <td width="70%">
-                                 <font face="arial" size="4" color="#ffffff">
-                                     <input type="text" size="40" />
-                                 </font>
-                                 </td>
-                            </tr>
-                            <tr>
-                                <td width="30%">
-                                    <font face="arial" size="4" color="#ffffff">
-                                        Message
-                                    </font>
-                                </td>
-                                <td width="70%">
-                                 <font face="arial" size="4" color="#ffffff">
-                                     <textarea rows="5" cols="42"></textarea>
-                                 </font>
-                                 </td>
-                            </tr>
-                            <tr>
-                                <td width="30%">
-                                   &nbsp;
-                                </td>
-                                <td width="70%">
-                                    <button type="Submit">
-                                        <font face="arial" size="4" color="#292929">
-                                             Submit
-                                        </font>
-                                </button>
-                                 </td>
-                            </tr>
-                           </table>
-                        </td>
-                        
+                        <td style={{color: 'white'}}>Email</td>
+                        <td><input type="email" name="email" id="email" /></td>
                     </tr>
                     <tr>
-                        <td colspan="3">
-                            &nbsp;
-                        </td>
+                        <td style={{color: 'white'}}>Message</td>
+                        <td><textarea name="message" id="message"></textarea></td>
                     </tr>
                 </table>
-            </td>
-
-        </tr>
-
-
-    </table> 
+                <input id='submit' type="submit" value="SUBMIT" />
+            </div>
         </div>
     )
 }
