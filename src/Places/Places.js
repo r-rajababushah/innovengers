4b835608e05f618264cdd643da90417f4f3703b0
Rajababu Shah <rajababushah.in@gmail.com>
2024-09-18 17:00:53 +0530

    U

--- src/Places/Places.js
+++ src/Places/Places.js
@@ -2,49 +2,49 @@ import "./Places.css"
 export default function Places(){
     return (
         <div className="placesC">
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place1.png" id="place1" className="placeImg" alt="#" />
                         <div className="container">
                             <h4><b>Mawlynnong, Meghalaya</b></h4> 
                         </div>
                 </div>    
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place2.png" id="place1" className="placeImg" alt="#" />
                         <div className="container">
                             <h4><b>Ziro, Arunachal Pradesh</b></h4> 
                         </div>
                 </div> 
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place3.png" id="place1" className="placeImg" alt="#"/>
                         <div className="container">
                             <h4><b>Majuli, Assam</b></h4> 
                         </div>
                 </div> 
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place4.png" id="place1" className="placeImg" alt="#"/>
                         <div className="container">
                             <h4><b>Munsiyari, Uttarakhand</b></h4> 
                         </div>
                 </div> 
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place5.png" id="place1" className="placeImg" alt="#" />
                         <div className="container">
                             <h4><b>Kalap, Uttarakhand:</b></h4> 
                         </div>
                 </div> 
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place6.png" id="place1" className="placeImg" alt="#" />
                         <div className="container">
                             <h4><b>Desal, Gujarat</b></h4> 
                         </div>
                 </div>   
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place7.png" id="place1" className="placeImg" alt="#"/>
                         <div className="container">
                             <h4><b>Cherrapunji, Meghalaya</b></h4> 
                         </div>
                 </div>  
-                <div className="cardP">
+                <div className="cardP placesCardi">
                         <img src="place7.png" id="place1" className="placeImg" alt="#" />
                         <div className="container">
                             <h4><b>Malana, Himachal Pradesh</b></h4> 
